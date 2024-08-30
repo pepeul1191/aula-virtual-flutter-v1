44de725995deef83ab65812e604455c2ee590d61
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-08-29 19:56:07 -0500

    usando colores para la aplicaion segun oscuro/claro

---
+++ lib/configs/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFFF7F2A);
+  static const Color secondaryColor = Color(0xFF615049);
+  static const Color backgroundColor = Color(0xFFF1D3BC);
+  static const Color textColor = Color(0xFF262F34);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,163 +1,91 @@
+import 'package:aula/configs/app_colors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        theme: _lightTheme(), // Tema claro
-        darkTheme: _darkTheme(), // Tema oscuro
-        themeMode:
-            ThemeMode.system, // Usa el tema según la configuración del sistema
-        primarySwatch: Colors.blue, // Color principal de la app
-        primaryColor: Colors.blueAccent, // Color del app bar, botones, etc.
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-
-  ThemeData _lightTheme() {
-    return ThemeData(
-      brightness: Brightness.light,
-      primarySwatch: Colors.blue,
-      primaryColor: Colors.blueAccent,
-      backgroundColor: Colors.white,
-      textTheme: TextTheme(
-        headline1: TextStyle(color: Colors.black87),
-        bodyText1: TextStyle(color: Colors.black),
-      ),
-      appBarTheme: AppBarTheme(
-        color: Colors.blue,
-        iconTheme: IconThemeData(color: Colors.white),
-        textTheme: TextTheme(
-          headline6: TextStyle(color: Colors.white, fontSize: 20),
+        primaryColor: AppColors.primaryColor, // Configuración del primaryColor
+        appBarTheme: AppBarTheme(
+          backgroundColor:
+              AppColors.primaryColor, // Color de fondo de la AppBar
+          foregroundColor:
+              Colors.white, // Color del texto e íconos de la AppBar
         ),
-      ),
-      buttonTheme: ButtonThemeData(
-        buttonColor: Colors.blueAccent,
-        textTheme: ButtonTextTheme.primary,
-      ),
-    );
-  }
-
-  ThemeData _darkTheme() {
-    return ThemeData(
-      brightness: Brightness.dark,
-      primarySwatch: Colors.blueGrey,
-      primaryColor: Colors.blueGrey[800],
-      backgroundColor: Colors.black,
-      textTheme: TextTheme(
-        headline1: TextStyle(color: Colors.white70),
-        bodyText1: TextStyle(color: Colors.white70),
-      ),
-      appBarTheme: AppBarTheme(
-        color: Colors.blueGrey[900],
-        iconTheme: IconThemeData(color: Colors.white),
-        textTheme: TextTheme(
-          headline6: TextStyle(color: Colors.white, fontSize: 20),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primaryColor,
+              foregroundColor:
+                  Colors.white // Color de fondo de los botones elevados
+              ),
         ),
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+        // Puedes definir otros aspectos del tema aquí
       ),
-      buttonTheme: ButtonThemeData(
-        buttonColor: Colors.blueGrey[700],
-        textTheme: ButtonTextTheme.primary,
+      darkTheme: ThemeData(
+        primaryColor: AppColors.primaryColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor:
+              AppColors.primaryColor, // Color de fondo de la AppBar
+          foregroundColor:
+              AppColors.textColor, // Color del texto e íconos de la AppBar
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primaryColor,
+              foregroundColor:
+                  AppColors.textColor // Color de fondo de los botones elevados
+              ),
+        ),
+        scaffoldBackgroundColor: AppColors.textColor,
+        // Puedes definir otros aspectos del tema aquí
       ),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final Color textColor = Theme.of(context).brightness == Brightness.dark
+        ? Colors.black // Color para el tema oscuro
+        : AppColors.backgroundColor; // Color para el tema claro
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Hola Mundo App'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Hola Mundo',
+              style: TextStyle(
+                fontSize: 24,
+                color: textColor,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Acción al presionar el botón
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Botón presionado')),
+                );
+              },
+              child: Text('Presiona aquí'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
